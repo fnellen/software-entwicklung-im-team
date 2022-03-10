203c5c82d3ec9cc0bc197e23705b8712ce93b8ce
dehus101 <Dennis.Husser@hhu.de>
2022-03-10 13:19:45 +0100

    Aggregat Student um eine Methode setzeUrlaub erweitert und KlausurReferenz in richtigen Ordner verschoben

---
+++ chicken_domain/src/main/java/chicken/aggregates/student/KlausurReferenz.java
@@ -0,0 +1,11 @@
+package chicken.aggregates.student;
+
+import chicken.aggregates.klausur.VeranstaltungsId;
+
+/**
+ * Referenziert Klausuren im Bezug zum Studenten.
+ */
+public record KlausurReferenz(VeranstaltungsId id) {
+
+
+}
