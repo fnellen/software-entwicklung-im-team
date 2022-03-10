84458338b6ea8871ce7c85eb5920dd57a75bd8a4
dehus101 <Dennis.Husser@hhu.de>
2022-03-10 13:21:27 +0100

    erste Methode zur Db Implementierung fertiggestellt

--- chicken_adapter_db/src/main/java/StudentRepositoryImpl.java
+++ chicken_adapter_db/src/main/java/StudentRepositoryImpl.java
@@ -1,12 +1,14 @@
 import chicken.aggregates.dto.ZeitraumDto;
+import chicken.aggregates.student.KlausurReferenz;
 import chicken.aggregates.student.Student;
+import dao.KlausurDao;
 import dao.StudentDao;
+import dto.KlausurReferenzDto;
 import dto.StudentDto;
 import dto.UrlaubZeitraumDto;
 import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import org.springframework.stereotype.Repository;
 import repositories.StudentRepository;
 
@@ -27,22 +29,14 @@ public class StudentRepositoryImpl implements StudentRepository {
   public Student findeStudentMitHandle(String githubHandle) {
     StudentDto studentDto = studentDao.findeStudentMitHandle(githubHandle)
         .orElseThrow(() -> new NoSuchElementException(githubHandle));
-    return konvertiereZuStudent(studentDto);
+    return studentDto.konvertiereZuStudent();
   }
 
-  private Student konvertiereZuStudent(StudentDto studentDto) {
-    Student student = new Student(studentDto.id(), studentDto.githubhandle());
-    Set<UrlaubZeitraumDto> urlaubZeitraumDtos = studentDto.urlaube();
-    Set<ZeitraumDto> zeitraumDtos =
-        urlaubZeitraumDtos.stream().map(UrlaubZeitraumDto::konvertiereZuZeitraumDto)
-            .collect(Collectors.toSet());
-    student.setzeUrlaube(zeitraumDtos);
-    //student.setzeKlausuren(studentDto.klausuren());
-    return student;
-  }
 
   @Override
   public void speicherStudent(Student student) {
 
   }
+
+
 }
