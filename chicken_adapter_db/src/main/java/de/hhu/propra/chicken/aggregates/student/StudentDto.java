92e8d3d3d86ee115836be339ce69137fa68b1a1b
pifis102 <pifis102@uni-duesseldorf.de>
2022-03-11 10:58:42 +0100

    Student zu StudentDto Konvertierung

--- chicken_adapter_db/src/main/java/de/hhu/propra/chicken/aggregates/student/StudentDto.java
+++ chicken_adapter_db/src/main/java/de/hhu/propra/chicken/aggregates/student/StudentDto.java
@@ -1,6 +1,7 @@
 package de.hhu.propra.chicken.aggregates.student;
 
 import de.hhu.propra.chicken.aggregates.dto.ZeitraumDto;
+import de.hhu.propra.chicken.aggregates.klausur.VeranstaltungsIdDto;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.springframework.data.annotation.Id;
@@ -25,4 +26,17 @@ public record StudentDto(@Id Long id,
     student.setzeKlausuren(klausurenReferenzen);
     return student;
   }
+
+  public static StudentDto konvertiereZuStudentDto(Student student) {
+    Set<UrlaubZeitraumDto> urlaubZeitraumDtos = student.getUrlaube().stream()
+        .map(e -> new UrlaubZeitraumDto(e.getDatum(), e.getStartUhrzeit(), e.getEndUhrzeit()))
+        .collect(Collectors.toSet());
+    Set<KlausurReferenzDto> klausurReferenzDtos = student.getKlausuren().stream()
+        .map(e -> new KlausurReferenzDto(new VeranstaltungsIdDto(e.id())))
+        .collect(Collectors.toSet());
+    return new StudentDto(student.getId(), student.getGithubHandle(), urlaubZeitraumDtos,
+        klausurReferenzDtos);
+  }
+
+
 }
