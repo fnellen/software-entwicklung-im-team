1287a87eaf33ac773fdf1133e4f163d968b98b3c
pifis102 <pifis102@uni-duesseldorf.de>
2022-03-15 10:49:45 +0100

    Konstruktor für die Exceptions gefixt

--- chicken_application/src/main/java/de/hhu/propra/chicken/services/fehler/StudentNichtGefundenException.java
+++ chicken_application/src/main/java/de/hhu/propra/chicken/services/fehler/StudentNichtGefundenException.java
@@ -2,5 +2,6 @@ package de.hhu.propra.chicken.services.fehler;
 
 public class StudentNichtGefundenException extends RuntimeException {
   public StudentNichtGefundenException(String githubHandle) {
+    super(githubHandle);
   }
 }
--- chicken_application/src/main/java/de/hhu/propra/chicken/services/fehler/UrlaubException.java
+++ chicken_application/src/main/java/de/hhu/propra/chicken/services/fehler/UrlaubException.java
@@ -2,5 +2,6 @@ package de.hhu.propra.chicken.services.fehler;
 
 public class UrlaubException extends RuntimeException {
   public UrlaubException(String fehler) {
+    super(fehler);
   }
 }
