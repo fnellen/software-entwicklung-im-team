c9abaf060d2263d62906e2c7f60f126fbe31a397
Federico Nellen <nellenfederico@gmail.com>
2022-03-15 15:56:56 +0100

    Zeitraum DTO und Klausur-Template Refactoring

--- chicken_application/src/test/java/de/hhu/propra/chicken/services/KlausurTemplate.java
+++ chicken_application/src/test/java/de/hhu/propra/chicken/services/KlausurTemplate.java
@@ -2,16 +2,23 @@ package de.hhu.propra.chicken.services;
 
 import static de.hhu.propra.chicken.services.ZeitraumDtoTemplate.ZEITRAUM_03_07_0930_1030;
 import static de.hhu.propra.chicken.services.ZeitraumDtoTemplate.ZEITRAUM_03_08_0930_1030;
+import static de.hhu.propra.chicken.services.ZeitraumDtoTemplate.ZEITRAUM_03_09_1000_1100;
 import static de.hhu.propra.chicken.services.ZeitraumDtoTemplate.ZEITRAUM_03_09_1130_1230;
 
 import de.hhu.propra.chicken.aggregates.klausur.Klausur;
 
 public class KlausurTemplate {
 
-  static final Klausur klausur1 = new Klausur("215783", "Propra2", ZEITRAUM_03_09_1130_1230, true);
-  static final Klausur klausur2 = new Klausur("214613", "Stochastik", ZEITRAUM_03_08_0930_1030,
-      true);
-  static final Klausur klausur3 = new Klausur("211234", "Rechnernetze", ZEITRAUM_03_07_0930_1030,
-      true);
+  static final Klausur KL_PROPRA_03_09_1130_1230 =
+      new Klausur("215783", "Propra2", ZEITRAUM_03_09_1130_1230, true);
+
+  static final Klausur KL_03_09_1000_1100 =
+      new Klausur("Random", "Random", ZEITRAUM_03_09_1000_1100, true);
+
+  static final Klausur KL_STOCHASTIK_03_08_0930_1030 =
+      new Klausur("214613", "Stochastik", ZEITRAUM_03_08_0930_1030, true);
+
+  static final Klausur KL_RECHNERNETZTE_03_07_0930_1030
+      = new Klausur("211234", "Rechnernetze", ZEITRAUM_03_07_0930_1030, true);
 
 }
--- chicken_application/src/test/java/de/hhu/propra/chicken/services/ZeitraumDtoTemplate.java
+++ chicken_application/src/test/java/de/hhu/propra/chicken/services/ZeitraumDtoTemplate.java
@@ -8,31 +8,98 @@ public class ZeitraumDtoTemplate {
 
 
   static final ZeitraumDto ZEITRAUM_03_07_0930_1030 = ZeitraumDto.erstelleZeitraum(
-      LocalDate.of(2022, 03, 7), LocalTime.of(9, 30),
+      LocalDate.of(2022, 3, 7),
+      LocalTime.of(9, 30),
       LocalTime.of(10, 30));
 
   static final ZeitraumDto ZEITRAUM_03_07_1230_1330 = ZeitraumDto.erstelleZeitraum(
-      LocalDate.of(2022, 03, 7), LocalTime.of(12, 30),
+      LocalDate.of(2022, 3, 7),
+      LocalTime.of(12, 30),
       LocalTime.of(13, 30));
 
   static final ZeitraumDto ZEITRAUM_03_07_1130_1230 = ZeitraumDto.erstelleZeitraum(
-      LocalDate.of(2022, 03, 7), LocalTime.of(11, 30),
+      LocalDate.of(2022, 3, 7),
+      LocalTime.of(11, 30),
       LocalTime.of(12, 30));
 
   static final ZeitraumDto ZEITRAUM_03_08_0930_1030 = ZeitraumDto.erstelleZeitraum(
-      LocalDate.of(2022, 03, 8), LocalTime.of(9, 30),
+      LocalDate.of(2022, 3, 8),
+      LocalTime.of(9, 30),
       LocalTime.of(10, 30));
 
   static final ZeitraumDto ZEITRAUM_03_08_1130_1230 = ZeitraumDto.erstelleZeitraum(
-      LocalDate.of(2022, 03, 8), LocalTime.of(11, 30),
+      LocalDate.of(2022, 3, 8),
+      LocalTime.of(11, 30),
       LocalTime.of(12, 30));
+
+  static final ZeitraumDto ZEITRAUM_03_09_0930_0945 = ZeitraumDto.erstelleZeitraum(
+      LocalDate.of(2022, 3, 9),
+      LocalTime.of(9, 30),
+      LocalTime.of(9, 45));
+
+  static final ZeitraumDto ZEITRAUM_03_09_0930_1200 = ZeitraumDto.erstelleZeitraum(
+      LocalDate.of(2022, 3, 9),
+      LocalTime.of(9, 30),
+      LocalTime.of(12, 0));
+
+  static final ZeitraumDto ZEITRAUM_03_09_0930_1215 = ZeitraumDto.erstelleZeitraum(
+      LocalDate.of(2022, 3, 9),
+      LocalTime.of(9, 30),
+      LocalTime.of(12, 15));
+
+  static final ZeitraumDto ZEITRAUM_03_09_0930_1330 = ZeitraumDto.erstelleZeitraum(
+      LocalDate.of(2022, 3, 9),
+      LocalTime.of(9, 30),
+      LocalTime.of(13, 30));
+
+  static final ZeitraumDto ZEITRAUM_03_09_0930_1130 = ZeitraumDto.erstelleZeitraum(
+      LocalDate.of(2022, 3, 9),
+      LocalTime.of(9, 30),
+      LocalTime.of(11, 30));
+
+  static final ZeitraumDto ZEITRAUM_03_09_1000_1100 = ZeitraumDto.erstelleZeitraum(
+      LocalDate.of(2022, 3, 9),
+      LocalTime.of(10, 0),
+      LocalTime.of(11, 0));
+
+  static final ZeitraumDto ZEITRAUM_03_09_1030_1130 = ZeitraumDto.erstelleZeitraum(
+      LocalDate.of(2022, 3, 9),
+      LocalTime.of(10, 30),
+      LocalTime.of(11, 30));
+
+  static final ZeitraumDto ZEITRAUM_03_09_1030_1145 = ZeitraumDto.erstelleZeitraum(
+      LocalDate.of(2022, 3, 9),
+      LocalTime.of(10, 30),
+      LocalTime.of(11, 45));
+
+  static final ZeitraumDto ZEITRAUM_03_09_1100_1130 = ZeitraumDto.erstelleZeitraum(
+      LocalDate.of(2022, 3, 9),
+      LocalTime.of(11, 0),
+      LocalTime.of(11, 30));
 
   static final ZeitraumDto ZEITRAUM_03_09_1130_1230 = ZeitraumDto.erstelleZeitraum(
-      LocalDate.of(2022, 03, 9), LocalTime.of(11, 30),
+      LocalDate.of(2022, 3, 9),
+      LocalTime.of(11, 30),
       LocalTime.of(12, 30));
 
+  static final ZeitraumDto ZEITRAUM_03_09_1145_1330 = ZeitraumDto.erstelleZeitraum(
+      LocalDate.of(2022, 3, 9),
+      LocalTime.of(11, 45),
+      LocalTime.of(13, 30));
+
+  static final ZeitraumDto ZEITRAUM_03_09_1300_1330 = ZeitraumDto.erstelleZeitraum(
+      LocalDate.of(2022, 3, 9),
+      LocalTime.of(13, 0),
+      LocalTime.of(13, 30));
+
+  static final ZeitraumDto ZEITRAUM_03_09_1230_1330 = ZeitraumDto.erstelleZeitraum(
+      LocalDate.of(2022, 3, 9),
+      LocalTime.of(12, 30),
+      LocalTime.of(13, 30));
+
   static final ZeitraumDto ZEITRAUM_03_14_1130_1230 = ZeitraumDto.erstelleZeitraum(
-      LocalDate.of(2022, 03, 14), LocalTime.of(11, 30),
+      LocalDate.of(2022, 3, 14),
+      LocalTime.of(11, 30),
       LocalTime.of(12, 30));
 
   static final ZeitraumDto ZEITRAUM_03_15_1030_1130 = ZeitraumDto.erstelleZeitraum(
