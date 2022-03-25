b260dc13879caa53ef4ecdca628894c89ceaf8cd
Federico Nellen <nellenfederico@gmail.com>
2022-03-25 13:23:34 +0100

    Ende zu Ende Urlaub belegen funktioniert

--- chicken_spring/src/test/java/de/hhu/propra/chicken/ChickenSystemTest.java
+++ chicken_spring/src/test/java/de/hhu/propra/chicken/ChickenSystemTest.java
@@ -4,10 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlButton;
+import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
 import com.gargoylesoftware.htmlunit.html.HtmlTable;
 import com.gargoylesoftware.htmlunit.html.HtmlTableCell;
 import com.gargoylesoftware.htmlunit.javascript.SilentJavaScriptErrorListener;
@@ -54,21 +56,29 @@ public class ChickenSystemTest {
   void test_1() throws Exception {
 
     HtmlPage page = webClient.getPage("http://localhost:8080/");
-    HtmlButton urlaubBelegenNavButton = page.getElementByName("urlaub-belegen-btn");
+    HtmlAnchor urlaubBelegenNavButton = page.getElementByName("urlaub-belegen-a");
     page = urlaubBelegenNavButton.click();
-    System.out.println(page);
     HtmlForm urlaubBelegenForm = page.getFormByName("urlaub_anmelden");
     HtmlInput urlaubsDatum = urlaubBelegenForm.getInputByName("urlaubsDatum");
-    urlaubsDatum.setValueAttribute("10.03.2022");
+    urlaubsDatum.setValueAttribute("2022-03-10");
     HtmlInput urlaubsStart = urlaubBelegenForm.getInputByName("urlaubsStart");
     urlaubsStart.setValueAttribute("10:00");
     HtmlInput urlaubsEnde = urlaubBelegenForm.getInputByName("urlaubsEnde");
     urlaubsEnde.setValueAttribute("11:00");
-    HtmlPage startSeite = urlaubBelegenForm.getInputByName("urlaubsanmeldung_abschicken").click();
-    HtmlTable urlaubsTabelle = startSeite.getElementByName("urlaub_table");
+
+    Iterable<DomElement> childElements = urlaubBelegenForm.getChildElements();
+    childElements.forEach(System.out::println);
+
+    HtmlSubmitInput button = urlaubBelegenForm.getInputByName("abschicken");
+
+    //urlaubBelegenForm.submit(button);
+
+    page = button.click();
+    System.out.println(page);
+    HtmlTable urlaubsTabelle = page.getElementByName("urlaub_table");
     HtmlTableCell urlaubsDatumZelle = urlaubsTabelle.getCellAt(1, 0);
     System.out.println(urlaubsDatumZelle);
-    assertThat(urlaubsDatumZelle.toString()).contains("2022-03-10");
+    assertThat(urlaubsDatumZelle.getTextContent()).contains("2022-03-10");
 
 //    HtmlForm versandmethodenwahl = page.getFormByName("urlaubStornieren");
 //    HtmlInput direktfahrt = versandmethodenwahl.getInputByValue("DIREKTFAHRT");
